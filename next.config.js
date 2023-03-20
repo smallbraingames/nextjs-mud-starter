50a645083c992e9c6015cef70f2147a62a9bd133
smallbraingames <smallbraindev@gmail.com>
2023-03-20 12:22:38 -0700

    feat: add mud client packages w/ correct transpilation

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
-module.exports = nextConfig
+const nextConfig = {
+  reactStrictMode: false,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false };
+    return config;
+  },
+};
+
+const withTM = require("next-transpile-modules")([
+  "@latticexyz/network",
+  "@latticexyz/react",
+  "@latticexyz/recs",
+  "@latticexyz/std-client",
+  "@latticexyz/utils",
+]);
+
+module.exports = withTM(nextConfig);
